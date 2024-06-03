f3acd47bc1d63b1f230be1738910f234b27c49b9
@hexlet/cli <support@hexlet.io>
2024-06-04 00:50:28 +0300

    submit java-web-ru/cookies

---
+++ java-web-ru/cookies/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.UsersController;
+import exercise.util.NamedRoutes;
+import io.javalin.rendering.template.JavalinJte;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        // BEGIN
+        app.post(NamedRoutes.usersPath(), UsersController::create);
+
+        app.get("/users/build", ctx -> ctx.render("users/build.jte"));
+        // END
+
+        app.get(NamedRoutes.userPath("{id}"), UsersController::show);
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
