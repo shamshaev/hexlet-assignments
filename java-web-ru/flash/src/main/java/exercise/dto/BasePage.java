2a320385e954f8fcf8129ec965e4f5354579140e
@hexlet/cli <support@hexlet.io>
2024-06-05 19:45:37 +0300

    submit java-web-ru/flash

---
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -0,0 +1,15 @@
+// BEGIN
+package exercise.dto;
+
+public class BasePage {
+    private String flash;
+
+    public String getFlash() {
+        return flash;
+    }
+
+    public void setFlash(String flash) {
+        this.flash = flash;
+    }
+}
+// END
