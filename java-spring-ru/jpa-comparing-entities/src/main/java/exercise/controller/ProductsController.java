ca4a9aabb3434422bd13f4e6eb017c8d43400aa2
@hexlet/cli <support@hexlet.io>
2024-07-12 23:15:19 +0300

    submit java-spring-ru/jpa-comparing-entities

---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,64 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.exception.ResourceAlreadyExistsException;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(path = "")
+    public List<Product> index() {
+        return productRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        if (productRepository.findAll().stream()
+                .anyMatch(p -> p.equals(product))) {
+            throw new ResourceAlreadyExistsException("Product with id " + product.getId() + " already exist");
+        }
+        productRepository.save(product);
+        return product;
+    }
+    // END
+
+    @GetMapping(path = "/{id}")
+    public Product show(@PathVariable long id) {
+        var product =  productRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        return product;
+    }
+
+    @PutMapping(path = "/{id}")
+    public Product update(@PathVariable long id, @RequestBody Product productData) {
+        var product =  productRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        product.setTitle(productData.getTitle());
+        product.setPrice(productData.getPrice());
+
+        productRepository.save(product);
+
+        return product;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable long id) {
+        productRepository.deleteById(id);
+    }
+}
